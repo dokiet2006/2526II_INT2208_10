27a99f43db36257e2228b67b6cfa8d76c7594592
Do Duc Kiet <dokiet2k6@gmail.com>
2026-05-24 11:53:01 +0700

    feat: implement loan approval decision logic

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
+import java.util.*;
 public class Main {
-    public static boolean isValidInput(int age, double income, int credit_ccore, char employment) {
+    public static boolean isValidInput(int age, double income, int credit_score, char employment) {
         return (age >= 18 && age <= 65)
                 && (income >= 5.0 && income <= 500.0)
-                && (credit_ccore >= 300 && credit_ccore <= 850)
+                && (credit_score >= 300 && credit_score <= 850)
                 && (employment == 'C' || employment == 'F');
     }
 
@@ -14,5 +15,60 @@ public class Main {
         } else {
             return "Low";
         }
+    }
+
+    public static String decideLoan(int age, double income, int credit_score, char employment) {
+        if (!isValidInput(age, income, credit_score, employment)) {
+            return "Invalid Input";
+        }
+
+        String risk = getRiskLevel(credit_score);
+
+        //Rule 1: high risk luon Reject
+        if (risk.equalsIgnoreCase("High")) {
+            return "REJECT";
+        }
+
+        //Rule 2: income < 15 trieu
+        if (income < 15.0) {
+            if (employment == 'F' || risk.equalsIgnoreCase("Medium")) {
+                return "REJECT";
+            } else if (employment == 'C' || risk.equalsIgnoreCase("Low")) {
+                return "MANUAL REVIEW";
+            }
+        }
+
+        //Rule 3: income >= 15 trieu
+        if (income >= 15.0) {
+            if (risk.equalsIgnoreCase("Medium") || risk.equalsIgnoreCase("Low")) {
+                if (employment == 'C') {
+                    return "APPROVE";
+                } else {
+                    return "MANUAL REVIEW";
+                }
+            }
+        }
+        return "Invalid Input";
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("age: ");
+        int age = sc.nextInt();
+
+        System.out.print("income (million VND): ");
+        double income = sc.nextDouble();
+
+        System.out.print("credit score: ");
+        int creditScore = sc.nextInt();
+
+        System.out.print("employment (C/F): ");
+        char employment = sc.next().toUpperCase().charAt(0);
+
+        String result = decideLoan(age, income, creditScore, employment);
+        System.out.println("Decision: " + result);
+
+        sc.close();
     }
 }

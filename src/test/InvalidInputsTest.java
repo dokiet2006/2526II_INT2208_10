72ead63fe87b17f0ba236e78d15eab58e862af8a
Do Duc Kiet <dokiet2k6@gmail.com>
2026-05-24 11:08:59 +0700

    refactor: reorganize test suite into InvalidInputsTest and TriangleLogicTest

---
+++ src/test/InvalidInputsTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import main.Main;
+
+public class InvalidInputsTest {
+
+    @Test void TC01() {
+        assertEquals("Invalid Input", Main.result(0,50,50));
+    }
+
+    @Test void TC02() {
+        assertEquals("Invalid Input", Main.result(101,50,50));
+    }
+
+    @Test void TC03() {
+        assertEquals("Invalid Input", Main.result(50,0,50));
+    }
+
+    @Test void TC04() {
+        assertEquals("Invalid Input", Main.result(50,101,50));
+    }
+
+}

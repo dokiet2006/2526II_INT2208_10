261ca899645426e67b1645ee092d3c5dde5decb4
Do Duc Kiet <dokiet2k6@gmail.com>
2026-05-27 14:25:52 +0700

    test: update triangle test cases

--- src/test/InvalidInputsTest.java
+++ src/test/InvalidInputsTest.java
@@ -7,20 +7,33 @@ import main.Main;
 
 public class InvalidInputsTest {
 
-    @Test void TC01() {
+    @Test
+    void TC01() {
         assertEquals("Invalid Input", Main.result(0,50,50));
     }
 
-    @Test void TC02() {
+    @Test
+    void TC02() {
         assertEquals("Invalid Input", Main.result(101,50,50));
     }
 
-    @Test void TC03() {
+    @Test
+    void TC03() {
         assertEquals("Invalid Input", Main.result(50,0,50));
     }
 
-    @Test void TC04() {
+    @Test
+    void TC04() {
         assertEquals("Invalid Input", Main.result(50,101,50));
     }
 
+    @Test
+    void TC05() {
+        assertEquals("Invalid Input", Main.result(50,50,0));
+    }
+
+    @Test
+    void TC06() {
+        assertEquals("Invalid Input", Main.result(50,50,101));
+    }
 }
--- src/test/TriangleLogicTest.java
+++ src/test/TriangleLogicTest.java
@@ -7,47 +7,47 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TriangleLogicTest {
     @Test
-    void TC05() {
-        assertEquals("Invalid Input", Main.result(50,50,0));
-    }
-
-    @Test void TC06() {
-        assertEquals("Invalid Input", Main.result(50,50,101));
-    }
-
-    @Test void TC07() {
+    void TC07() {
         assertEquals("Not a Triangle", Main.result(10,20,50));
     }
 
-    @Test void TC08() {
+    @Test
+    void TC08() {
         assertEquals("Not a Triangle", Main.result(1,2,3));
     }
 
-    @Test void TC09() {
+    @Test
+    void TC09() {
         assertEquals("Equilateral", Main.result(50,50,50));
     }
 
-    @Test void TC10() {
+    @Test
+    void TC10() {
         assertEquals("Equilateral", Main.result(100,100,100));
     }
 
-    @Test void TC11() {
+    @Test
+    void TC11() {
         assertEquals("Isosceles", Main.result(50,50,40));
     }
 
-    @Test void TC12() {
+    @Test
+    void TC12() {
         assertEquals("Isosceles", Main.result(40,50,50));
     }
 
-    @Test void TC13() {
+    @Test
+    void TC13() {
         assertEquals("Isosceles", Main.result(50,40,50));
     }
 
-    @Test void TC14() {
+    @Test
+    void TC14() {
         assertEquals("Scalene", Main.result(3,4,5));
     }
 
-    @Test void TC15() {
+    @Test
+    void TC15() {
         assertEquals("Scalene", Main.result(98,99,100));
     }
 }
